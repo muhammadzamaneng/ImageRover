28ee248a5dbfd550b220da3e101da9fdaf839710
muhammadzamaneng <muhammad.zaman.engg.@gmail.com>
2023-10-08 14:19:40 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import HomeScreen from './Screens/Home';
+
+function App(): JSX.Element {
+  return <HomeScreen />;
+}
+
+export default App;
---
+++ src/Screens/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function HomeScreen(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Image Rover</Text>
+    </SafeAreaView>
+  );
+}
+
+export default HomeScreen;
